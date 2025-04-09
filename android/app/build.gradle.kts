3e8f2c0db99e368a71218d737dd89740fb5eafb8
rasakumarvivekan <rasakumarvivekan@gmail.com>
2025-04-09 06:36:16 +0000

    chore:commit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,7 +2,9 @@ plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
+    id("dev.flutter.flutter-gradle-plugin")    
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -41,4 +43,10 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+  // Import the Firebase BoM
+  implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
+
 }
